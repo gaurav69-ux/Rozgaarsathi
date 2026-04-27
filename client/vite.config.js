c4e427f6d1d9acbbff13e5c6ee5a23b379630b76
Gaurav Durge <gauravdurge35@gmail.com>
2026-04-27 12:23:37 +0530

    makes some changes

--- client/vite.config.js
+++ client/vite.config.js
@@ -8,6 +8,12 @@ export default defineConfig({
   server: {
     host: '0.0.0.0',   // ← bind to all interfaces
     port: 5173,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000',
+        changeOrigin: true,
+      },
+    },
   },
 })
 
